8ca038aebea28d360ac8906f8a62414073ba68f2
stembrino <stembrino@gmail.com>
2023-06-22 14:30:18 +0100

    fix style svg

--- src/SlotProductCard/Header/Header.tsx
+++ src/SlotProductCard/Header/Header.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./styles.module.css";
 import { HeaderProps } from "./types";
 import Icon from "@mdi/react";
-import { mdiHeartOutline, mdiHeart } from "@mdi/js";
+import { mdiHeartOutline } from "@mdi/js";
 
-const Header: React.FC<HeaderProps> = ({ onClick }) => {
+const Header: React.FC<HeaderProps> = () => {
   return (
     <div className={styles.base}>
       <Icon path={mdiHeartOutline} size={1} />
